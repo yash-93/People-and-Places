d8a5ee247d9b3dcd0579a9fc364460f070d0da66
yash-93 <yashdeepbachhas93@gmail.com>
2020-05-09 01:12:25 +0530

    NavBar and NavLinks added

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-d
 
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 function App() {
   return (
     <Router>
-    <Switch>
-      <Route path="/" exact>
-        <Users />
-      </Route>
-      <Route path="/places/new" exact>
-        <NewPlace />
-      </Route>
-      <Redirect to="/">
-      </Redirect>
-    </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/">
+          </Redirect>
+        </Switch>
+      </main>
   </Router>
   );
 };
