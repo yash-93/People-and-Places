5ef66df940745c21ca34d2c22a8fa7f15b8f6c83
yash-93 <yashdeepbachhas93@gmail.com>
2020-05-08 21:04:58 +0530

    Routing of User Page without styling done

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+
+function App() {
+  return (
+    <Router>
+    <Switch>
+      <Route path="/" exact>
+        <Users />
+      </Route>
+      <Route path="/places/new" exact>
+        <NewPlace />
+      </Route>
+      <Redirect to="/">
+      </Redirect>
+    </Switch>
+  </Router>
+  );
+};
+
+export default App;
