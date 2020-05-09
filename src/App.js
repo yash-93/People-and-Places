6bde7e46889a9ffd13b35ff1a237531073b3254a
yash-93 <yashdeepbachhas93@gmail.com>
2020-05-09 16:51:04 +0530

    Filtering user's places

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
-import Users from './user/pages/Users';
-import NewPlace from './places/pages/NewPlace';
-import MainNavigation from './shared/components/Navigation/MainNavigation';
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+import UserPlaces from "./places/pages/UserPlaces";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 function App() {
   return (
@@ -14,15 +20,17 @@ function App() {
           <Route path="/" exact>
             <Users />
           </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
+          </Route>
           <Route path="/places/new" exact>
             <NewPlace />
           </Route>
-          <Redirect to="/">
-          </Redirect>
+          <Redirect to="/"></Redirect>
         </Switch>
       </main>
-  </Router>
+    </Router>
   );
-};
+}
 
 export default App;
