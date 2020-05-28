3d7425a34e18cceb4dfa545325a97701a9bf6ee5
yash-93 <yashdeepbachhas93@gmail.com>
2020-05-29 01:10:23 +0530

    User Routes added

---
+++ backend/controllers/users-controller.js
@@ -0,0 +1,49 @@
+const { v4: uuidv4 } = require("uuid");
+const HttpError = require("../models/http-error");
+
+const DUMMY_PLACES = [
+  {
+    id: "u1",
+    name: "Yashdeep Bachhas",
+    email: "yash@gmail.com",
+    password: "1397",
+  },
+];
+
+const getUsers = (req, res, next) => {
+  res.json({ users: DUMMY_PLACES });
+};
+
+const signup = (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const hasUser = DUMMY_PLACES.find((u) => u.email === email);
+  if (hasUser) {
+    throw new HttpError("User already exists.", 422);
+  }
+
+  const createdUser = {
+    id: uuidv4(),
+    name,
+    email,
+    password,
+  };
+
+  DUMMY_PLACES.push(createdUser);
+  res.status(201).json({ user: createdUser });
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  const identifiedUser = DUMMY_PLACES.find((u) => u.email === email);
+  if (!identifiedUser || identifiedUser.password !== password) {
+    throw new HttpError("Wrong Credentials.", 401);
+  }
+
+  res.json({ message: "Logged in" });
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
