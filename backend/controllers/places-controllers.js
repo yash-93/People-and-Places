fc220cad10bb3df13ba71f45d6bc34282ed15683
yash-93 <yashdeepbachhas93@gmail.com>
2020-05-30 15:49:10 +0530

    Updating existing place in MongoDB

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -101,23 +101,34 @@ const createPlace = async (req, res, next) => {
   res.status(201).json({ place: createdPlace });
 };
 
-const updatePlace = (req, res, next) => {
+const updatePlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    // console.log(errors);
     throw new HttpError("Invalid inputs passed.", 422);
   }
 
   const { title, description } = req.body;
   const placeId = req.params.pid;
-  const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
-  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
-  updatedPlace.title = title;
-  updatedPlace.description = description;
 
-  DUMMY_PLACES[placeIndex] = updatedPlace;
+  let place;
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError("Something went wrong, unable to update.", 500);
+    return next(error);
+  }
 
-  res.status(200).json({ place: updatedPlace });
+  place.title = title;
+  place.description = description;
+
+  try {
+    await place.save();
+  } catch (err) {
+    const error = new HttpError("Something went wrong, unable to update.", 500);
+    return next(error);
+  }
+
+  res.status(200).json({ place });
 };
 
 const deletePlace = (req, res, next) => {
