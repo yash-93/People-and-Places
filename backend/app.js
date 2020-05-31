16a0cdb85b000ad93f567efaf20ab8eebf04da5f
yash-93 <yashdeepbachhas93@gmail.com>
2020-05-31 20:05:54 +0530

    added middleware to allow CORS

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 app.use("/api/places", placesRoutes);
 
 app.use("/api/users", usersRoutes);
@@ -29,7 +39,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb+srv://yashdeep:1397@cluster0-fpul6.mongodb.net/places?retryWrites=true&w=majority",
+    "mongodb+srv://yashdeep:1397@cluster0-fpul6.mongodb.net/mern?retryWrites=true&w=majority",
     { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
   )
   .then(() => {
