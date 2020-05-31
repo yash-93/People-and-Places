737cfeb3034ee495153f0ef12ddbaf4ee71b3e13
yash-93 <yashdeepbachhas93@gmail.com>
2020-06-01 02:37:35 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback } from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
-  Switch,
-} from "react-router-dom";
+  Switch
+} from 'react-router-dom';
 
-import Users from "./user/pages/Users";
-import NewPlace from "./places/pages/NewPlace";
-import UserPlaces from "./places/pages/UserPlaces";
-import UpdatePlace from "./places/pages/UpdatePlace";
-import Auth from "./user/pages/Auth";
-import MainNavigation from "./shared/components/Navigation/MainNavigation";
-import { AuthContext } from "./shared/context/auth-context";
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
+import UpdatePlace from './places/pages/UpdatePlace';
+import Auth from './user/pages/Auth';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+import { AuthContext } from './shared/context/auth-context';
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
--- frontend/src/shared/components/Navigation/NavLinks.js
+++ frontend/src/shared/components/Navigation/NavLinks.js
@@ -1,10 +1,10 @@
-import React, { useContext } from "react";
-import { NavLink } from "react-router-dom";
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
 
-import { AuthContext } from "../../context/auth-context";
-import "./NavLinks.css";
+import { AuthContext } from '../../context/auth-context';
+import './NavLinks.css';
 
-const NavLinks = (props) => {
+const NavLinks = props => {
   const auth = useContext(AuthContext);
 
   return (
