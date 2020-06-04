2a10347895ade7f6ea1b4ed222c3ec8645b0a3eb
yash-93 <yashdeepbachhas93@gmail.com>
2020-06-05 03:02:48 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -14,29 +14,24 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use("/uploads/images", express.static(path.join("uploads", "images")));
-app.use(express.static(path.join("public")));
 
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
 
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+  const error = new HttpError("Could ot find this route", 404);
+  throw error;
 });
-
-// app.use((req, res, next) => {
-//   const error = new HttpError("Could ot find this route", 404);
-//   throw error;
-// });
 
 app.use((error, req, res, next) => {
   if (req.file) {
